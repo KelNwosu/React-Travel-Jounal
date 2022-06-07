7536c1fe5551f06584ae527f99808f9a74b70bdb
Kelechi Nwosu <kjpn00@gmail.com>
2022-06-07 00:10:13 -0600

    initial commit

---
+++ src/navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Navbar() {
+    return (
+        <div className='banner'>
+            <h1>my travel journal.</h1>
+
+
+
+        </div>
+    )
+}
